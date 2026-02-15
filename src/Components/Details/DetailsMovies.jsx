7ee36356a2a17c08ce483f3f8c0922c76ffbdba7
mariameid <mariameid210@gmail.com>
2026-02-16 01:26:04 +0200

    removed unused imports and added CI=false in package .json buils script

--- src/Components/Details/DetailsMovies.jsx
+++ src/Components/Details/DetailsMovies.jsx
@@ -1,41 +1,40 @@
-import axios from 'axios'
-import React from 'react'
-import { useEffect,useState } from 'react';
-import { imgPath } from '../../constant/imgPath';
-import { useLocation, useParams } from 'react-router-dom';
-
-
-
+import axios from "axios";
+import React from "react";
+import { useEffect, useState } from "react";
+import { imgPath } from "../../constant/imgPath";
+import { useLocation, useParams } from "react-router-dom";
 
 export default function DetailsMovies() {
-  const location=useLocation();
+  const location = useLocation();
   const [details, setDetails] = useState(null);
-  let {id}=useParams();
-  let detype= location.pathname.includes("movie")&& "movie";
-  
-  function getDetails(){
-    const apiKey = '8fd8b99a7a13ae3f94b8d8288b84ad49';  
-    const url = `https://api.themoviedb.org/3/${detype}/${id}?api_key=${apiKey}`;
-  
-    axios.get(url)
-    .then((res)=>{
-      setDetails(res.data);
-    }).catch((err)=>{console.log(err)});
+  let { id } = useParams();
+  let detype = location.pathname.includes("movie") && "movie";
 
+  function getDetails() {
+    const apiKey = "8fd8b99a7a13ae3f94b8d8288b84ad49";
+    const url = `https://api.themoviedb.org/3/${detype}/${id}?api_key=${apiKey}`;
+
+    axios
+      .get(url)
+      .then((res) => {
+        setDetails(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
   useEffect(() => {
     getDetails();
-  }, [])
-  
+  }, []);
 
   return (
-  <div className="row">
-    <div className="col-md-3">
-<img src= {imgPath(details?.poster_path)} alt=""className='w-100 ' />
+    <div className="row">
+      <div className="col-md-3">
+        <img src={imgPath(details?.poster_path)} alt="" className="w-100 " />
+      </div>
+      <div className="col-md-8 offset-1">
+        <p>{details?.overview}</p>
+      </div>
     </div>
-    <div className="col-md-8 offset-1">
-      <p>{  details?.overview }</p>
-    </div>
-  </div>
-  )
+  );
 }
--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,74 +1,72 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 
-import "./style.css"
-import { imgPath } from '../../constant/imgPath';
-import { Link } from 'react-router-dom';
-import PersonCard from './components/PersonCard.jsx';
-import MovieCard from './components/Moviecard.jsx';
-import TvCard from './components/Tvcard.jsx';
-import { ContextMovies } from '../Store/index.jsx';
+import "./style.css";
+
+import PersonCard from "./components/PersonCard.jsx";
+import MovieCard from "./components/Moviecard.jsx";
+import TvCard from "./components/Tvcard.jsx";
+import { ContextMovies } from "../Store/index.jsx";
 
 export default function Home() {
-const {movies,people,tv}=useContext(ContextMovies);
- 
+  const { movies, people, tv } = useContext(ContextMovies);
+
   return (
     <>
-    <div className="row gy-5">
-
-<div className="col-md-4 d-flex align-items-center">
-<div>
-<div className='brdr w-25'></div>
-<h3 className='fw-bold'>Trending<br/> Movies<br/>To watch right now  </h3>
-<p className='textHome'>Top Trending movies day by day</p>
-<div className='brdr'></div>
-</div>
-</div>   
-{
-
-movies.slice(0,10).map((movie,i)=>(
-
-<MovieCard movie={movie} key={i}/>
-))
-}
- </div>
- {/* people */}
- <div className="row mt-5">
-
-<div className="col-md-4 d-flex align-items-center">
-<div>
-<div className='brdr w-25'></div>
-<h3 className='fw-bold'>Trending<br/> Actors <br/>To view right now  </h3>
-<p className='textHome'>Top Trending Actors day by day</p>
-<div className='brdr'></div>
-</div>
-</div>   
-{
-
-people.slice(0,10).map((person,i)=>(
- <PersonCard key={i} person={person}/>
-))
-}
- </div>
- {/* tv */}
- <div className="row mt-5">
-
-<div className="col-md-4 d-flex align-items-center">
-<div>
-<div className='brdr w-25'></div>
-<h3 className='fw-bold'>Trending<br/> Tv<br/>To view right now  </h3>
-<p className='textHome'>Top Trending tv day by day</p>
-<div className='brdr'></div>
-</div>
-</div>   
-{
-
-tv.slice(0,10).map((tv,i)=>(
-  
- <TvCard key={i} tv={tv}/>
-))
-}
- </div>
-
+      <div className="row gy-5">
+        <div className="col-md-4 d-flex align-items-center">
+          <div>
+            <div className="brdr w-25"></div>
+            <h3 className="fw-bold">
+              Trending
+              <br /> Movies
+              <br />
+              To watch right now{" "}
+            </h3>
+            <p className="textHome">Top Trending movies day by day</p>
+            <div className="brdr"></div>
+          </div>
+        </div>
+        {movies.slice(0, 10).map((movie, i) => (
+          <MovieCard movie={movie} key={i} />
+        ))}
+      </div>
+      {/* people */}
+      <div className="row mt-5">
+        <div className="col-md-4 d-flex align-items-center">
+          <div>
+            <div className="brdr w-25"></div>
+            <h3 className="fw-bold">
+              Trending
+              <br /> Actors <br />
+              To view right now{" "}
+            </h3>
+            <p className="textHome">Top Trending Actors day by day</p>
+            <div className="brdr"></div>
+          </div>
+        </div>
+        {people.slice(0, 10).map((person, i) => (
+          <PersonCard key={i} person={person} />
+        ))}
+      </div>
+      {/* tv */}
+      <div className="row mt-5">
+        <div className="col-md-4 d-flex align-items-center">
+          <div>
+            <div className="brdr w-25"></div>
+            <h3 className="fw-bold">
+              Trending
+              <br /> Tv
+              <br />
+              To view right now{" "}
+            </h3>
+            <p className="textHome">Top Trending tv day by day</p>
+            <div className="brdr"></div>
+          </div>
+        </div>
+        {tv.slice(0, 10).map((tv, i) => (
+          <TvCard key={i} tv={tv} />
+        ))}
+      </div>
     </>
-  )
+  );
 }
